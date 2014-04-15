f00ce94ab84677a16c49b9cef9f2c54063b12c38
VooleDev6 <taojigu@163.com>
2014-04-15 18:37:52 +0800

    bomb view layout finished

---
+++ BombLiar/BombLiar/ViewController/LiarWeb1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  LiarWeb1ViewController.h
+//  BombLiar
+//
+//  Created by VooleDev6 on 14-4-15.
+//  Copyright (c) 2014年 VooleDev6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiarWeb1ViewController : UIViewController
+
+@end
---
+++ BombLiar/BombLiar/ViewController/LiarWeb1ViewController.m
@@ -0,0 +1,38 @@
+//
+//  LiarWeb1ViewController.m
+//  BombLiar
+//
+//  Created by VooleDev6 on 14-4-15.
+//  Copyright (c) 2014年 VooleDev6. All rights reserved.
+//
+
+#import "LiarWeb1ViewController.h"
+
+@interface LiarWeb1ViewController ()
+
+@end
+
+@implementation LiarWeb1ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
